917238d3e7ac16dcbb308005d20145bca2be37d5
christianjunior05 <bchrist.junior@gmail.com>
2024-08-27 10:20:29 +0000

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
 const data = [
   {
-    image: '/images/jordan.jpeg',
-    title : 'Micheal jordan ',
+    image: "/images/jordan.jpeg",
+    title: "Micheal jordan ",
   },
   {
-    image: '/images/ljames.jpeg',
-    title : 'Lebron James ',
+    image: "/images/ljames.jpeg",
+    title: "Lebron James ",
   },
   {
-    image: '/images/giannis.jpeg',
-    title : 'Giannis Antetokoumpo ',
-  }
-]
+    image: "/images/giannis.jpeg",
+    title: "Giannis Antetokoumpo ",
+  },
+];
 
 function App() {
   return (
     <div className="h-screen bg-slate-800 pt-10">
-    <div className="h-[400px] w-3/4 m-auto border">Hello</div>
+      <div className="h-[400px] w-3/4 m-auto border">
+        {
+          data.map((item, index) => {
+            return (
+              <div key={index}>
+                <div>
+                  <img src={item.image} alt={`Image ${index}`} />
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+
+export default App;
